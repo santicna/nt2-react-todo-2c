bb7d262e9c372c2278b465868a4139bd34d15d8c
Santiago Cartagena <scartagena147@gmail.com>
2021-09-18 16:43:57 -0300

    Todo list react completo sin css

--- src/App.js
+++ src/App.js
@@ -1,40 +1,100 @@
-import React, { Component } from 'react';
-import Tareas from './Tareas'
+import React, { Component } from "react";
+import Tareas from "./Tareas";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-        tareas:[]
+      tareas: [],
+      filteredTareas: [],
+      filtered: false,
     };
   }
 
   addTodo() {
-    const tarea = {
-      titulo : prompt ("Tarea a realizar"),
-      checked:false
+    const input = prompt("Tarea a realizar");
+    if (input === null || input == "") {
+      return;
     }
-
-    const newArray = Array.from(this.state.tareas)
-    newArray.push(tarea)
-    this.setState({tareas:newArray})
+    let tarea = {
+      key: Math.random(),
+      titulo: input,
+      checked: false,
+      setChecked: this.handleCheckBox.bind(this),
+    };
+    const newArray = Array.from(this.state.tareas);
+    newArray.push(tarea);
+    this.setState({ tareas: newArray });
   }
 
+  deleteAllChecked() {
+    const allTareas = Array.from(this.state.tareas);
+    const newArray = allTareas.filter((tarea) => !tarea.checked);
+    this.setState({ tareas: newArray });
+  }
+  handleCheckBox(key) {
+    const newArray = Array.from(this.state.tareas);
+    let tarea = newArray.find((tarea) => tarea.key === key);
+    tarea.checked = !tarea.checked;
+    this.setState({ tareas: newArray });
+  }
+  deleteTarea(key) {
+    const newArray = Array.from(this.state.tareas);
+    this.setState({ tareas: newArray.filter((tarea) => tarea.key != key) });
+  }
+  handleFilter() {
+    const texto = document.getElementById(123).value;
+    if (texto === "") {
+      this.setState({ filteredTareas: [], filtered: false });
+    } else {
+      const newArray = this.state.tareas.filter((tarea) =>
+        tarea.titulo.includes(texto)
+      );
+      this.setState({ filteredTareas: newArray, filtered: true });
+    }
+  }
 
   render() {
     return (
       <div>
-      <h1 class="center title">TODO App</h1>
-      <div class="flow-right controls">
-
-        <span>Item count: <span id="item-count">0</span></span>
-        <span>Unchecked count: <span id="unchecked-count">0</span></span>
-      </div>
-      <button class="button center" onClick={this.addTodo.bind(this)}>Agregar Tarea</button>
-
-
-        <Tareas tareas={this.state.tareas}/>
-
+        <h1 className="center title">TODO App</h1>
+        <div className="flow-right controls">
+          <span>
+            Item count: <span id="item-count">{this.state.tareas.length}</span>
+          </span>
+          <span>
+            Unchecked count:{" "}
+            <span id="unchecked-count">
+              {
+                this.state.tareas.filter((tarea) => tarea.checked == false)
+                  .length
+              }
+            </span>
+          </span>
+        </div>
+        <button className="button center" onClick={this.addTodo.bind(this)}>
+          Agregar Tarea
+        </button>
+        <button
+          className="button center"
+          onClick={this.deleteAllChecked.bind(this)}
+        >
+          Eliminar terminadas
+        </button>
+        <label>Filtro: </label>
+        <input
+          id="123"
+          type="text"
+          label="filtro"
+          onKeyUp={this.handleFilter.bind(this)}
+        ></input>
+        <Tareas
+          className="todo list"
+          tareas={
+            this.state.filtered ? this.state.filteredTareas : this.state.tareas
+          }
+          deleteTarea={this.deleteTarea.bind(this)}
+        />
       </div>
     );
   }
