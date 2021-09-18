2a158f82b9d77f874633627931642e947a5b1e8b
Santiago Cartagena <scartagena147@gmail.com>
2021-09-18 16:44:41 -0300

    completo

--- src/Tarea.js
+++ src/Tarea.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default function Tarea(props) {
-return (
-<li>
-  <p style = {{color:'blue'}}>
-{props.texto}
-</p>
-</li>
-)
+  function check() {
+    props.tarea.setChecked(props.tarea.key);
+  }
+  function handleDelete() {
+    props.deleteTarea(props.tarea.key);
+  }
+  return (
+    <li>
+      <p style={{ textDecoration: "" /*"line-through"*/ }}>
+        {props.tarea.titulo}
+        <input type="checkbox" onChange={check}></input>
+        <button type="todo-delete" onClick={handleDelete}>
+          ❌
+        </button>
+      </p>
+    </li>
+  );
 }
--- src/Tareas.js
+++ src/Tareas.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import Tarea from './Tarea'
-
+import React, { Component } from "react";
+import Tarea from "./Tarea";
 
 export default function Tareas(props) {
-
-  return(
-    <ul id="todo-list" class="todo-list">       
-    {  props.tareas.map((tarea)=> {
-      return  <Tarea texto={tarea.titulo}/>
-    })
-    }
+  return (
+    <ul id="todo-list" className="todo-list">
+      {props.tareas.map((tarea) => {
+        return (
+          <Tarea
+            key={tarea.key}
+            tarea={tarea}
+            deleteTarea={props.deleteTarea}
+          />
+        );
+      })}
     </ul>
-
-
-  )
+  );
 }
